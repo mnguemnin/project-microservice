fff8ec21c2ae8f6e9bd1d7adee35eb45987989b2
mnguemnin <137334019+mnguemnin@users.noreply.github.com>
2026-01-20 23:22:04 +0100

    initialize project + creation of product ms

---
+++ product-service/src/main/java/com/techie/microservice/product/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.techie.microservice.product.controller;
+
+import com.techie.microservice.product.dto.ProductRequest;
+import com.techie.microservice.product.dto.ProductResponse;
+import com.techie.microservice.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
