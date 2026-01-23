e62625add372911cbfca2f521467f648af36b616
mnguemnin <137334019+mnguemnin@users.noreply.github.com>
2026-01-23 12:32:58 +0100

    updating ms

--- order-service/src/main/java/com/techie/microservice/order/service/OrderService.java
+++ order-service/src/main/java/com/techie/microservice/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.techie.microservice.order.service;
 
+import com.techie.microservice.order.client.InventoryClient;
 import com.techie.microservice.order.dto.OrderRequest;
 import com.techie.microservice.order.model.Order;
 import com.techie.microservice.order.repository.OrderRepository;
@@ -15,13 +16,13 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
-   // private final InventoryClient inventoryClient;
+    private final InventoryClient inventoryClient;
    // private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
 
-        //var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
-        //if (isProductInStock) {
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
             order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
@@ -38,8 +39,8 @@ public class OrderService {
             log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
             kafkaTemplate.send("order-placed", orderPlacedEvent);
             log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
-       *//* } else {
+       */ } else {
             throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
-        }*/
+        }
     }
 }
